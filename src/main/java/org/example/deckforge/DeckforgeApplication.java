cd38000398b81a7708032394236f7fd23f506d7c
Sofj05 <soj011@edu.zealand.dk>
2026-05-12 09:30:04 +0200

    Initial commit

---
+++ src/main/java/org/example/deckforge/DeckforgeApplication.java
@@ -0,0 +1,13 @@
+package org.example.deckforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckforgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeckforgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/deckforge/DeckforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.deckforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckforgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
